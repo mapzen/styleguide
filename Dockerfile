ee4e79b00575e0040bc2142bcc800eb4a8a4d757
Evan Griffiths <griffithse@gmail.com>
2016-10-10 17:20:31 -0400

    testing docker on circle

--- Dockerfile
+++ Dockerfile
@@ -4,12 +4,4 @@ RUN mkdir -p /app
 WORKDIR /app
 ADD package.json .
 
-RUN apk add --update alpine-sdk python && \
-    npm install -g gulp-cli http-server && \
-    npm install && \
-    apk del --purge alpine-sdk python
-
-COPY . .
-RUN gulp build
-
-CMD http-server /app/dist
+CMD bash
--- docker-build.sh
+++ docker-build.sh
@@ -5,5 +5,5 @@ repo="$1"
 if [ -f "~/docker/image.tar" ]; then docker load -i ~/docker/image.tar; fi
 docker build --rm=false -t $repo .
 mkdir -p ~/docker
-docker save $repo > ~/docker/image.tar
-# docker save $(docker history -q $repo | sed '/^<missing>$/d') > ~/docker/image.tar
+# docker save $repo > ~/docker/image.tar
+docker save $(docker history -q $repo | sed '/^<missing>$/d') > ~/docker/image.tar
