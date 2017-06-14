63a77137101a4350f2465c181e0130c847be0f77
Hanbyul Jo <hanbyul@nyu.edu>
2017-06-14 17:48:15 -0400

    added tooltip and popover

---
+++ src/scripts/bootstrap-components.js
@@ -0,0 +1,24 @@
+// Initializing bootstrap components (Tooltip, Popover)
+//
+// Dependencies: JQuery, Bootstrap
+// --------------------------------------------------------
+(function (root, factory) {
+  // Universal Module Definition (UMD)
+  // via https://github.com/umdjs/umd/blob/master/templates/returnExports.js
+  if (typeof module === 'object' && module.exports) {
+    // Node. Does not work with strict CommonJS, but
+    // only CommonJS-like environments that support module.exports,
+    // like Node.
+    module.exports = factory();
+  } else {
+    // Browser globals (root is window)
+    root.returnExports = factory();
+  }
+}(this, function () {
+  'use strict'
+
+  $('[data-toggle="popover"]').popover();
+  $('[data-toggle="tooltip"]').tooltip();
+
+  return {};
+}));
